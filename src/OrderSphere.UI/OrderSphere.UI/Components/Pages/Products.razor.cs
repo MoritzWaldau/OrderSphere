c09ea99100a280335612730b6092cf7da55a3b5c
Moritz Waldau <moritzwaldau99@gmail.com>
2026-03-29 14:01:02 +0200

    Save

--- src/OrderSphere.UI/OrderSphere.UI/Components/Pages/Products.razor.cs
+++ src/OrderSphere.UI/OrderSphere.UI/Components/Pages/Products.razor.cs
@@ -10,9 +10,9 @@ namespace OrderSphere.UI.Components.Pages;
 
 public partial class Products : OrderSphereComponentBase
 {
-    
-
     private IEnumerable<ProductDto> _products = [];
+    private bool _popupOpen = false;
+    private ProductDto? _selectedProduct;
 
     protected override async Task LoadDataAsync()
     {
@@ -22,5 +22,25 @@ public partial class Products : OrderSphereComponentBase
             onSuccess: products => _products = products,
             onFailure: error => Snackbar.Add(error.Description, Severity.Error)
         );
+    }
+
+    private void OpenQuantityPopup(ProductDto product)
+    {
+        _selectedProduct = product;
+        _popupOpen = true;
+    }
+
+    private void ClosePopup()
+    {
+        _popupOpen = false;
+        _selectedProduct = null;
+    }
+
+    private void ConfirmAddToCart(int quantity)
+    {
+        if (_selectedProduct is null) return;
+
+        Snackbar.Add($"{quantity}x {_selectedProduct.Name} wurde zum Warenkorb hinzugefügt.", Severity.Success);
+        ClosePopup();
     }
 }

6a9b2273d8795a15338a2a4fac85b38f7d2501c3
Moritz Waldau <moritzwaldau99@gmail.com>
2026-03-29 13:42:31 +0200

    Change base loading

--- src/OrderSphere.UI/OrderSphere.UI/Components/Pages/Products.razor.cs
+++ src/OrderSphere.UI/OrderSphere.UI/Components/Pages/Products.razor.cs
@@ -4,39 +4,23 @@ using MudBlazor;
 using OrderSphere.Application.Features.Product.GetProduct;
 using OrderSphere.Application.Models;
 using OrderSphere.Domain.Extensions;
+using OrderSphere.UI.Configuration;
 
 namespace OrderSphere.UI.Components.Pages;
 
-public partial class Products
+public partial class Products : OrderSphereComponentBase
 {
-    [Inject] 
-    public required ISender Sender { get; set; }
+    
 
     private IEnumerable<ProductDto> _products = [];
-    private bool _loading = true;
-    private bool _firstRender = true;
 
-    protected override async Task OnInitializedAsync()
+    protected override async Task LoadDataAsync()
     {
-        // Beim PreRender nichts laden, Skeleton zeigen
-        if (_firstRender) return;
-
         var result = await Sender.Send(new GetProductQuery());
 
         result.Match(
             onSuccess: products => _products = products,
             onFailure: error => Snackbar.Add(error.Description, Severity.Error)
         );
-
-        _loading = false;
-    }
-
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (!firstRender) return;
-
-        _firstRender = false;
-        await OnInitializedAsync();
-        StateHasChanged();
     }
 }
---
+++ src/OrderSphere.UI/OrderSphere.UI/Configuration/OrderSphereComponentBase.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+
+namespace OrderSphere.UI.Configuration;
+
+public abstract class OrderSphereComponentBase : ComponentBase
+{
+    [Inject] public required ISender Sender { get; set; }
+
+    private bool _firstRender = true;
+    protected bool IsLoading { get; private set; } = true;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (_firstRender) return;
+        await LoadDataAsync();
+        IsLoading = false;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!firstRender) return;
+        _firstRender = false;
+        await OnInitializedAsync();
+        StateHasChanged();
+    }
+
+    protected virtual Task LoadDataAsync() => Task.CompletedTask;
+}

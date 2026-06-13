39977b05e4e7620e287922681361ab8c246875c9
Moritz Waldau <moritzwaldau99@gmail.com>
2026-06-14 00:53:59 +0200

    Save

--- src/Services/Ordering/OrderSphere.Ordering.Application/Abstractions/IOrderingDbContext.cs
+++ src/Services/Ordering/OrderSphere.Ordering.Application/Abstractions/IOrderingDbContext.cs
@@ -8,6 +8,8 @@ public interface IOrderingDbContext
     DbSet<Order> Orders { get; }
     DbSet<OrderItem> OrderItems { get; }
 
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
     Task BeginTransactionAsync(CancellationToken ct = default);
     Task CommitAsync(CancellationToken ct = default);
     Task RollbackAsync(CancellationToken ct = default);
